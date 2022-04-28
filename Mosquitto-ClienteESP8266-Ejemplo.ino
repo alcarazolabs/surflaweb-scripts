c1c3c56546ff4f8a4aba9d2bc50dd5c17cc20497
Freddy Alcarazo <freddyalcarazo@hotmail.com>
2022-04-28 11:26:47 -0500

    Update Mosquitto-ClienteESP8266-Ejemplo.ino

--- Mosquitto-ClienteESP8266-Ejemplo.ino
+++ Mosquitto-ClienteESP8266-Ejemplo.ino
@@ -16,6 +16,8 @@ const char *ssid="WLAN_WAL -1";
 const char *passwd = "942918777";
 
 char serial_command = -1;
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
 
 WiFiClient wlanclient;
 PubSubClient mqttClient(wlanclient);
@@ -56,6 +58,9 @@ void setup() {
   Serial.println();
   Serial.println ("Connected to WiFi AP, Got an IP address :");
   Serial.println (WiFi.localIP());
+  //Set The ESP8266 tries to reconnect automatically when the connection is lost
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
 
   mqttClient.setServer (server, port);
   mqttClient.setCallback(mqttCallback);
@@ -79,6 +84,9 @@ void setup() {
 
 void loop() {
 
+ 
+  printWifiStatus();
+
   if(Serial.available()>0){
 
      serial_command = Serial.read();
@@ -93,6 +101,8 @@ void loop() {
    if(!mqttClient.connected()) {
       reconnect();
   }
+
+
   
   mqttClient.loop();
 }
@@ -118,5 +128,29 @@ void reconnect() {
       // Wait 5 seconds before retrying
       delay(5000);
     }
+    printWifiStatus();
   }
 }
+
+
+void printWifiStatus(){
+      //print the Wi-Fi status every 30 seconds
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >=interval){
+      switch (WiFi.status()){
+        case WL_NO_SSID_AVAIL:
+          Serial.println("Wifi Configured SSID cannot be reached");
+          break;
+        case WL_CONNECTED:
+          Serial.println("Connection Wifi successfully established");
+          break;
+        case WL_CONNECT_FAILED:
+          Serial.println("Wifi Connection failed");
+          break;
+      }
+      Serial.printf("Connection status: %d\n", WiFi.status());
+      Serial.print("RRSI: ");
+      Serial.println(WiFi.RSSI());
+      previousMillis = currentMillis;
+  }
+  }
