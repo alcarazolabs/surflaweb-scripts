9856a6dd2997466402c19033d3d61887fe08e8c3
Freddy Alcarazo <freddyalcarazo@hotmail.com>
2022-05-19 10:14:48 -0500

    Update upload_big_csv_with_dask.py

--- upload_big_csv_with_dask.py
+++ upload_big_csv_with_dask.py
@@ -2,20 +2,29 @@
 import dask.dataframe as dd
 import time
 
+
+"""
+dataframe = dd.read_csv("my_csv_file.csv", 
+                        dtype={'DNI': 'str', 'AP_PAT': 'str', 'AP_MAT': 'str','NOMBRES': 'str'},
+                        low_memory=False)
+                        
+"""
 start_time = time.time()
-dataframe = dd.read_csv("csv.csv", dtype={'myColumn1': 'str',
-                                    'myColumn2': 'str',
-                                    'myColumn_n': 'str'
-                                    }) # Important to specify the datatype of your columns of your csv
+dataframe = dd.read_csv("my_csv_file.csv",
+                        low_memory=False,
+                        dtype=str)
+# dtype=str lee todo el dataframe diciendo que todo los tipos de datos de todas las columnas son strings.
+# Si se desea especificar el tipo de dato de cada columna se debe de hacer para cada una como arriba linea 7.
+# essto es util por que dask o pandas creara la columna en la bd en base a este tipo de dato.
 
 print("dask.dataframe took %s seconds" %(time.time() - start_time))
 
 
 # Enviar csv a la base de datos
 start_time = time.time()
-dataframe.to_sql('persons', uri='mysql://root:123456@localhost/apidb', if_exists='replace', index=False)
+dataframe.to_sql('persons', uri='mysql://root:123456@localhost/myapidb', if_exists='replace', index=False)
 print("Realizado en  %s segundos" %(time.time() - start_time))
 
 # Dask creara la tabla primero y luego de unos minutos empezara a escribir los datos.
 
-# https://docs.dask.org/en/latest/generated/dask.dataframe.to_sql.html
+
