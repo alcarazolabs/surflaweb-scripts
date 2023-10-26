628236e07ba5446a2e75a8eb598d0f0bda6276a9
Freddy Alcarazo <freddyalcarazo@hotmail.com>
2023-10-26 18:35:58 -0500

    Add files via upload

---
+++ test_insightface.py
@@ -0,0 +1,11 @@
+import numpy as np
+import os
+import glob
+import cv2
+import matplotlib.pyplot as plt
+
+import insightface
+from insightface.app import FaceAnalysis
+from insightface.data import get_image as ins_get_image
+
+print("done!")
