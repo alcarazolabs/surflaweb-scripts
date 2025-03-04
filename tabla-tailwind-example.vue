883227a3057975d8229591e74e12369174286a22
Freddy Alcarazo <freddyalcarazo@hotmail.com>
2025-03-04 11:49:11 -0500

    Add files via upload

---
+++ tabla-tailwind-example.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
+        <div class="min-w-full align-middle">
+            <table class="min-w-full divide-y divide-gray-200 border">
+                <thead>
+                <tr>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>
+                    </th>
+                </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                <tr>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">1</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">A</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Poe switched his focus to prose, and spent the next several years working for literary journals and periodicals, becoming known for his own style of literary criticism. His work forced him to move between several cities, including Baltimore, Philadelphia, and New York City. In 1836, when he was 27, he married his 13-year-old cousin, Virginia Clemm. She died of tuberculosis in 1847. In January 1845, he published his poem "The Raven" to instant success. He planned for years to produce his own journal The Penn, later renamed The Stylus. But before it began publishing, Poe died in Baltimore in 1849, aged 40, under mysterious circumstances. The cause of his death remains unknown, and has been variously attributed to many causes including disease, alcoholism, substance abuse, and suicide.</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2024-06-01 13:43:47</td>
+                </tr>
+                <tr>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">B</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Poe switched his focus to prose, and spent the next several years working for literary journals and periodicals, becoming known for his own style of literary criticism. His work forced him to move between several cities, including Baltimore, Philadelphia, and New York City. In 1836, when he was 27, he married his 13-year-old cousin, Virginia Clemm. She died of tuberculosis in 1847. In January 1845, he published his poem "The Raven" to instant success. He planned for years to produce his own journal The Penn, later renamed The Stylus. But before it began publishing, Poe died in Baltimore in 1849, aged 40, under mysterious circumstances. The cause of his death remains unknown, and has been variously attributed to many causes including disease, alcoholism, substance abuse, and suicide.</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2024-06-02 14:43:47</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
